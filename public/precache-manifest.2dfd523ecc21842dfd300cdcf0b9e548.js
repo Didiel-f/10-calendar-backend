1a56e7062839d55c0b030569d8679c84414e5ebb
Didiel-f <didiel.f@gmail.com>
2020-08-20 18:08:34 -0400

    v1 CalendarApp

---
+++ public/precache-manifest.2dfd523ecc21842dfd300cdcf0b9e548.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40aba5f069c5bacf7e98eec5e1e727da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdec8282b74050c563af",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "76b29364cbc8422b9137",
+    "url": "/static/css/main.88358507.chunk.css"
+  },
+  {
+    "revision": "cdec8282b74050c563af",
+    "url": "/static/js/2.5e0b88ad.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.5e0b88ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b29364cbc8422b9137",
+    "url": "/static/js/main.5ead072d.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
